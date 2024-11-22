66b476b6117ce385855a8dbdc30472b12345e6e1
Kevin Moisch <kevinmoisch@gmail.com>
2024-11-22 02:18:27 +0100

    Fix: Endpoints

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from fastapi import Depends, FastAPI, HTTPException, Request, status
 from fastapi.middleware.cors import CORSMiddleware
-from cookie import suno_auth, keep_alive
 
 import schemas
+from deps import get_token
 from utils import (
     generate_lyrics,
     generate_music,
@@ -11,22 +11,6 @@ from utils import (
     concat_music,
     get_feeds,
 )
-
-def get_token():
-    try:
-        keep_alive(suno_auth)
-        token = suno_auth.get_token()
-        if not token:
-            raise HTTPException(
-                status_code=status.HTTP_401_UNAUTHORIZED,
-                detail="Authentication token not available.",
-            )
-        return token
-    except Exception as e:
-        raise HTTPException(
-            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-            detail=f"Error retrieving token: {str(e)}",
-        )
 
 
 app = FastAPI()
@@ -48,89 +32,82 @@ async def get_root():
 
 @app.post("/generate")
 async def generate(
-    data: schemas.CustomModeGenerateParam
+    data: schemas.CustomModeGenerateParam, token: str = Depends(get_token)
 ):
-    token = get_token()
     try:
-        data_response = await generate_music(data.dict(), token)
-        return data_response
+        resp = await generate_music(data.dict(), token)
+        return resp
     except Exception as e:
         raise HTTPException(
             detail=str(e), status_code=status.HTTP_500_INTERNAL_SERVER_ERROR
         )
 
 
-# @app.post("/generate/description-mode")
-# async def generate_with_song_description(
-#     data: schemas.DescriptionModeGenerateParam, token: str = Depends(get_token)
-# ):
-#     token = get_token()
-#     try:
-#         resp = await generate_music(data.dict(), token)
-#         return resp
-#     except Exception as e:
-#         raise HTTPException(
-#             detail=str(e), status_code=status.HTTP_500_INTERNAL_SERVER_ERROR
-#         )
+@app.post("/generate/description-mode")
+async def generate_with_song_description(
+    data: schemas.DescriptionModeGenerateParam, token: str = Depends(get_token)
+):
+    try:
+        resp = await generate_music(data.dict(), token)
+        return resp
+    except Exception as e:
+        raise HTTPException(
+            detail=str(e), status_code=status.HTTP_500_INTERNAL_SERVER_ERROR
+        )
 
 
-# @app.get("/feed/{aid}")
-# async def fetch_feed(aid: str):
-#     token = get_token()
-#     try:
-#         resp = await get_feed(aid, token)
-#         return resp
-#     except Exception as e:
-#         raise HTTPException(
-#             detail=str(e), status_code=status.HTTP_500_INTERNAL_SERVER_ERROR
-#         )
+@app.get("/feed/{aid}")
+async def fetch_feed(aid: str, token: str = Depends(get_token)):
+    try:
+        resp = await get_feed(aid, token)
+        return resp
+    except Exception as e:
+        raise HTTPException(
+            detail=str(e), status_code=status.HTTP_500_INTERNAL_SERVER_ERROR
+        )
 
 
-# @app.get("/feeds/{aid}")
-# async def fetch_feed(aid: str):
-#     token = get_token()
-#     try:
-#         resp = await get_feeds(aid, token)
-#         return resp
-#     except Exception as e:
-#         raise HTTPException(
-#             detail=str(e), status_code=status.HTTP_500_INTERNAL_SERVER_ERROR
-#         )
+@app.get("/feeds/{aid}")
+async def fetch_feed(aid: str, token: str = Depends(get_token)):
+    try:
+        resp = await get_feeds(aid, token)
+        return resp
+    except Exception as e:
+        raise HTTPException(
+            detail=str(e), status_code=status.HTTP_500_INTERNAL_SERVER_ERROR
+        )
 
 
-# @app.post("/generate/lyrics/")
-# async def generate_lyrics_post(
-#     data: schemas.GenerateLyricsParam
-# ):
-#     token = get_token()
-#     try:
-#         resp = await generate_lyrics(data.prompt, token)
-#         return resp
-#     except Exception as e:
-#         raise HTTPException(
-#             detail=str(e), status_code=status.HTTP_500_INTERNAL_SERVER_ERROR
-#         )
+@app.post("/generate/lyrics/")
+async def generate_lyrics_post(
+    data: schemas.GenerateLyricsParam, token: str = Depends(get_token)
+):
+    try:
+        resp = await generate_lyrics(data.prompt, token)
+        return resp
+    except Exception as e:
+        raise HTTPException(
+            detail=str(e), status_code=status.HTTP_500_INTERNAL_SERVER_ERROR
+        )
 
 
-# @app.get("/lyrics/{lid}")
-# async def fetch_lyrics(lid: str):
-#     token = get_token()
-#     try:
-#         resp = await get_lyrics(lid, token)
-#         return resp
-#     except Exception as e:
-#         raise HTTPException(
-#             detail=str(e), status_code=status.HTTP_500_INTERNAL_SERVER_ERROR
-#         )
+@app.get("/lyrics/{lid}")
+async def fetch_lyrics(lid: str, token: str = Depends(get_token)):
+    try:
+        resp = await get_lyrics(lid, token)
+        return resp
+    except Exception as e:
+        raise HTTPException(
+            detail=str(e), status_code=status.HTTP_500_INTERNAL_SERVER_ERROR
+        )
 
 
-# @app.post("/generate/concat")
-# async def concat(data: schemas.ConcatParam):
-#     token = get_token()
-#     try:
-#         resp = await concat_music(data.dict(), token)
-#         return resp
-#     except Exception as e:
-#         raise HTTPException(
-#             detail=str(e), status_code=status.HTTP_500_INTERNAL_SERVER_ERROR
-#         )
+@app.post("/generate/concat")
+async def concat(data: schemas.ConcatParam, token: str = Depends(get_token)):
+    try:
+        resp = await concat_music(data.dict(), token)
+        return resp
+    except Exception as e:
+        raise HTTPException(
+            detail=str(e), status_code=status.HTTP_500_INTERNAL_SERVER_ERROR
+        )
